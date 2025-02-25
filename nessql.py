13f938fb5c321384b22c0712b77a9835d7c5cdb7
syyntax <syyntax@protonmail.com>
2025-02-25 13:07:33 -0600

    Updated SQL

--- nessql.py
+++ nessql.py
@@ -70,12 +70,12 @@ def get_statistics(db_path):
     total_hosts = cursor.fetchone()[0]
     
     cursor.execute("""
-        SELECT severity, COUNT(*) FROM vulnerabilities
+        SELECT severity, COUNT(DISTINCT(plugin_name)) FROM vulnerabilities
         GROUP BY severity ORDER BY severity DESC;
     """)
     severity_counts = {row[0]: row[1] for row in cursor.fetchall()}
     
-    cursor.execute("SELECT port, COUNT(*) AS count FROM open_ports GROUP BY port ORDER BY count DESC LIMIT 10;")
+    cursor.execute("SELECT port, protocol, COUNT(DISTINCT(host_id)) AS count FROM open_ports WHERE NOT port = 0 GROUP BY port ORDER BY count DESC LIMIT 10;")
     top_ports = cursor.fetchall()
     
     cursor.execute("SELECT ip FROM hosts LIMIT 1;")
